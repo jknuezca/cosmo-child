beed0c00cab8016d25bf14577adf4cf8bb5be38b
jknuezca <nuezcajorge@gmail.com>
2023-08-10 09:48:20 +0400

    added ACF for Hero Section

--- front-page.php
+++ front-page.php
@@ -8,6 +8,20 @@ get_header();
 ?>
 
 <main class="main-layout">
+    <section id="hero">
+        <div class="hero-main" role="main">
+            <?php while ( have_posts() ) : the_post(); ?>
+                <div class="hero-content">
+                    <h1><?php the_field('hero_title'); ?></h1>
+                    <p><?php the_field('hero_description'); ?></p>
+                </div>
+                <div class="hero-image">
+                    <img src="<?php the_field('hero_image'); ?>" alt="" />
+                </div>
+            <?php endwhile;  ?>
+        </div>
+    </section>
+
     <section class="match-list-container">
         <div id="match-list">
             <!-- Match items will be dynamically inserted here -->
