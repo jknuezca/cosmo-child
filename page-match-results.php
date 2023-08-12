ec4a9b21edd7dc20c4bdc8c42950c23aee176552
jknuezca <nuezcajorge@gmail.com>
2023-08-12 15:26:02 +0400

    group matches by tournament name

--- page-match-results.php
+++ page-match-results.php
@@ -1,6 +1,5 @@
 <?php
-// Assuming you have a way to fetch all unique dates from your matches.
-    $dates = get_all_match_dates();
+    //Fetch all unique dates from matches
     $all_dates = get_all_match_dates();
     
     get_header();
@@ -9,11 +8,11 @@
 <div class="match-tabs-container">
     <div class="tabs">
         <?php 
-        // Display tab buttons
-        foreach ($all_dates as $date) {
-            // Create button for the tab
-            echo '<button class="tablink" onclick="openMatchDate(event, \'' . $date . '\')">' . $date . '</button>';
-        } 
+            // Display tab buttons
+            foreach ($all_dates as $date) {
+                // Create button for the tab
+                echo '<button class="tablink" onclick="openMatchDate(event, \'' . $date . '\')">' . $date . '</button>';
+            } 
         ?>
     </div>
 
@@ -21,55 +20,69 @@
     // Display match details
     foreach ($all_dates as $date): ?>
         <div id="<?php echo $date; ?>" class="tabcontent">
-            <table class="match-results-table">
-                <thead>
-                    <tr>
-                        <th>Tournament</th>
-                        <th>Home Team</th>
-                        <th>Away Team</th>
-                        <th>Home Score</th>
-                        <th>Away Score</th>
-                        <!-- Add more columns as needed -->
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php 
-                    // Fetch matches for this date and sort them by tournament.
-                    $matches = get_matches_by_date_sorted_by_tournament($date);
-                    foreach ($matches as $match): 
-                    ?>
+            <?php 
+            // Fetch matches for this date sorted by tournament.
+            $matches = get_matches_by_date_sorted_by_tournament($date);
+            
+            // Group matches by tournament
+            $grouped_matches = [];
+            foreach ($matches as $match) {
+                $tournament_name = $match['tournament_name'];
+                if (!isset($grouped_matches[$tournament_name])) {
+                    $grouped_matches[$tournament_name] = [];
+                }
+                $grouped_matches[$tournament_name][] = $match;
+            }
+            
+            // Now display matches grouped by tournament
+            foreach ($grouped_matches as $tournament => $tournament_matches): ?>
+
+                <table class="match-results-table">
+                    <thead>
                         <tr>
-                            <td><?php echo $match['tournament_name']; ?></td>
-                            <td><?php echo $match['home']; ?></td>
-                            <td><?php echo $match['away']; ?></td>
-                            <td><?php echo $match['home_score']; ?></td>
-                            <td><?php echo $match['away_score']; ?></td>
-                            <!-- Add more data as needed -->
+                            <th colspan="4"><?php echo $tournament; ?></th>
                         </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
+                        <!-- <tr>
+                            <th>Home Team</th>
+                            <th>Home Score</th>
+                            <th>Away Score</th>
+                            <th>Away Team</th>
+                        </tr> -->
+                    </thead>
+                    <tbody>
+                        <?php foreach ($tournament_matches as $match): ?>
+                            <tr>
+                                <td><?php echo $match['home']; ?></td>
+                                <td><?php echo $match['home_score']; ?></td>
+                                <td><?php echo $match['away_score']; ?></td>
+                                <td><?php echo $match['away']; ?></td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+
+            <?php endforeach; ?>
+
         </div>
     <?php endforeach; ?>
 </div>
 
-<?php
+<?php 
     get_footer();
 ?>
 
 <script>
-    function openMatchDate(evt, date) {
-        var i, tabcontent, tablinks;
-        tabcontent = document.getElementsByClassName("tabcontent");
-        
-        for (i = 0; i < tabcontent.length; i++) {
-            tabcontent[i].style.display = "none";
-        }
-        tablinks = document.getElementsByClassName("tablink");
-        for (i = 0; i < tablinks.length; i++) {
-            tablinks[i].className = tablinks[i].className.replace(" active", "");
-        }
-        document.getElementById(date).style.display = "block";
-        evt.currentTarget.className += " active";
+function openMatchDate(evt, date) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
     }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(date).style.display = "block";
+    evt.currentTarget.className += " active";
+}
 </script>
