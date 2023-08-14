9d6bbecd75664afd589a59dd4b42a32aac331f4a
jknuezca <nuezcajorge@gmail.com>
2023-08-14 14:29:15 +0400

    reverse dates displayed, optimized script

--- page-match-results.php
+++ page-match-results.php
@@ -8,6 +8,8 @@
         array_pop($dates_to_display);  // Remove the last date
         array_unshift($dates_to_display, date("Y-m-d")); // Add current date to the beginning
     }
+
+    $dates_to_display = array_reverse($dates_to_display);
     
     get_header();
 
@@ -16,8 +18,10 @@
 <main id="match-results" class="main-layout">
     <div class="match-tabs-container">
         <div class="tabs">
-            <?php 
-                $activeDate = reset($dates_to_display);  // Get the first date (should be current date)
+            <?php
+                // Get the first date (should be current date)
+                // $activeDate = reset($dates_to_display);  
+                $activeDate = date("Y-m-d");
                 foreach ($dates_to_display as $index => $date): 
                     $activeClass = $date == $activeDate ? 'active' : '';
             ?>
@@ -33,7 +37,7 @@
             ?>
             <div id="<?php echo $date; ?>" class="tabcontent" style="display: <?php echo $displayStyle; ?>">
                 <?php 
-                // Fetch matches for this date sorted by tournament.
+                // Fetch matches for this date sorted by tournament
                 $matches = get_matches_by_tournament($date);
                 
                 // Group matches by tournament
@@ -46,58 +50,77 @@
                     $grouped_matches[$tournament_name][] = $match;
                 }
                 
-                // Now display matches grouped by tournament
-                foreach ($grouped_matches as $tournament => $tournament_matches): ?>
+                if (empty($grouped_matches)) {
+                    echo "<p>No match results</p>";
+                } else {
+                    // Display matches grouped by tournament
+                    foreach ($grouped_matches as $tournament => $tournament_matches): ?>
 
-                    <table class="match-results-table">
-                        <thead>
-                            <tr>
-                                <th colspan="4"><?php echo $tournament; ?></th>
-                            </tr>
-                            <tr>
-                                <th>Home Team</th>
-                                <th>Home Score</th>
-                                <th>Away Score</th>
-                                <th>Away Team</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php foreach ($tournament_matches as $match): ?>
+                        <table class="match-results-table">
+                            <thead>
                                 <tr>
-                                    <td><?php echo $match['home']; ?></td>
-                                    <td><?php echo $match['home_score']; ?></td>
-                                    <td><?php echo $match['away_score']; ?></td>
-                                    <td><?php echo $match['away']; ?></td>
+                                    <th colspan="4"><?php echo $tournament; ?></th>
                                 </tr>
-                            <?php endforeach; ?>
-                        </tbody>
-                    </table>
+                                <!-- <tr>
+                                    <th>Home Team</th>
+                                    <th>Home Score</th>
+                                    <th>Away Score</th>
+                                    <th>Away Team</th>
+                                </tr> -->
+                            </thead>
+                            <tbody>
+                                <?php foreach ($tournament_matches as $match): ?>
+                                    <tr>
+                                        <td><?php echo $match['home']; ?></td>
+                                        <td><?php echo $match['home_score']; ?></td>
+                                        <td><?php echo $match['away_score']; ?></td>
+                                        <td><?php echo $match['away']; ?></td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
 
-                <?php endforeach; ?>
+                    <?php endforeach; 
+                }
+                ?>
 
             </div>
-        <?php endforeach; ?>
+        <?php endforeach;?>
     </div>
 </main>
+
 <?php 
     get_footer();
 ?>
 
 <script>
     function openMatchDate(evt, date) {
-        var i, tabcontent, tablinks;
-
-        tabcontent = document.getElementsByClassName("tabcontent");
-        for (i = 0; i < tabcontent.length; i++) {
-            tabcontent[i].style.display = "none";
-        }
-
-        tablinks = document.getElementsByClassName("tablink");
-        for (i = 0; i < tablinks.length; i++) {
-            tablinks[i].className = tablinks[i].className.replace(" active", "");
-        }
+        hideAllElementsByClassName("tabcontent");
+        deactivateAllElementsByClassName("tablink");
         
-        document.getElementById(date).style.display = "block";
-        evt.currentTarget.className += " active";
+        displayElementById(date);
+        activateCurrentElement(evt);
+    }
+
+    function hideAllElementsByClassName(className) {
+        const elements = document.getElementsByClassName(className);
+        for (const element of elements) {
+            element.style.display = "none";
+        }
+    }
+
+    function deactivateAllElementsByClassName(className) {
+        const elements = document.getElementsByClassName(className);
+        for (const element of elements) {
+            element.classList.remove("active");
+        }
+    }
+
+    function displayElementById(id) {
+        document.getElementById(id).style.display = "block";
+    }
+
+    function activateCurrentElement(evt) {
+        evt.currentTarget.classList.add("active");
     }
 </script>
