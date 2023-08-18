0a0268ee4da4c4c9ca17c1081f0cd5bba7c68d4c
jknuezca <nuezcajorge@gmail.com>
2023-08-18 12:47:21 +0400

    optimized code for api-functions.php

--- api-functions.php
+++ api-functions.php
@@ -1,29 +1,31 @@
 <?php
 
-function fetch_api_data() {
-    // Set the API endpoint URL
-    $api_url = 'https://686sports.com/matchListWeb';
+    function fetch_api_data() {
+        $api_url = 'https://686sports.com/matchListWeb';
 
-    // Make the API request
-    $response = wp_remote_get($api_url);
+        $response = wp_remote_get($api_url);
 
-    // Check for errors
-    if (is_wp_error($response)) {
-        return false; // Return false to indicate an error occurred
+        // Check if there was an error with the HTTP request
+        if (is_wp_error($response)) {
+            error_log('API fetch error: ' . $response->get_error_message());
+            return false;
+        }
+
+        // Retrieve and validate the response body
+        $response_body = wp_remote_retrieve_body($response);
+        if ($response_body === '') {
+            error_log('API fetch error: Empty response.');
+            return false;
+        }
+
+        // Decode and validate the JSON data
+        $data = json_decode($response_body, true);
+        if ($data === null || !is_array($data) || empty($data)) {
+            error_log('API fetch error: Invalid JSON or empty data.');
+            return false;
+        }
+
+        return $data;
     }
-
-    // Retrieve the response body
-    $response_body = wp_remote_retrieve_body($response);
-
-    // Decode the JSON response
-    $data = json_decode($response_body, true);
-
-    // Check if decoding was successful and the API response contains data
-    if (!$data || !is_array($data) || empty($data)) {
-        return false; // Return false to indicate an empty or invalid API response
-    }
-
-    return $data; // Return the decoded API data
-}
 
 ?>
